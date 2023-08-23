f0ac1cb884f3704c8add241090b8e659ac7a58e0
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-08-22 22:00:32 -0300

    config link props

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
 
-const LinkWrapper = () => (
+type LinkWrapperProps = {
+  href: string
+  children: React.ReactNode
+}
+
+const LinkWrapper = ({ href, children }: LinkWrapperProps) => (
   <>
-    <Link href="#" className="text-wai-green hover:underline focus:outline-offset-8 focus:outline-blue-700 visited:text-wai-green">
+    <Link href={href} className="text-wai-green hover:underline focus:outline-offset-8 focus:outline-blue-700 visited:text-wai-green">
       <h2 className="flex items-start">
-        <span>What is Lorem Ipsum?</span>
+        <span>{children}</span>
       </h2>
     </Link>
   </>
--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -5,17 +5,29 @@ const Tiles = () => (
     <ul className="grid gap-y-10 m-4">
       <li className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
         <span>22 de ago, 2023</span>
-        <LinkWrapper/>
+        <LinkWrapper href="/post">
+          <h2>
+            <span>Read This First</span>
+          </h2>
+        </LinkWrapper>
         <div className="flex-grow">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.</div>
       </li>
       <li className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
         <span>22 de ago, 2023</span>
-        <LinkWrapper/>
+        <LinkWrapper href="/post">
+          <h2>
+            <span>Read This First</span>
+          </h2>
+        </LinkWrapper>
         <div className="flex-grow">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.</div>
       </li>
       <li className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
         <span>22 de ago, 2023</span>
-        <LinkWrapper/>
+        <LinkWrapper href="/post">
+          <h2>
+            <span>Read This First</span>
+          </h2>
+        </LinkWrapper>
         <div className="flex-grow">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English.</div>
       </li>
     </ul>
