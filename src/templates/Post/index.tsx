604f91e6c7343edbf6e0980b45162699499d568d
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-08-24 11:32:08 -0300

    adjusting template Post

--- src/templates/Post/index.tsx
+++ src/templates/Post/index.tsx
@@ -1,32 +1,18 @@
 import Image from 'next/image'
+import LinkWrapper from '@/components/Link'
 
 const PostTemplate = () => (
 <>
   <article className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
+    <LinkWrapper href="/">
+      <h2>
+        <span>Return</span>
+      </h2>
+    </LinkWrapper>
     <header className="">
       <h1>title rarirama</h1>
       <span>Wednesday June 1, 2022</span>
     </header>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi sit amet felis
-      fringilla, a consequat elit cursus. Fusce et magna eget libero elementum ultricies at
-      nec urna. Nulla facilisi.
-    </p>
-    <p>
-      Quisque tincidunt, nunc in efficitur congue, orci urna ultrices nisl, vel tincidunt
-      tortor sapien non mi. Proin facilisis nisi et purus congue, in eleifend lorem volutpat.
-      Curabitur at purus sit amet ligula iaculis tincidunt.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi sit amet felis
-      fringilla, a consequat elit cursus. Fusce et magna eget libero elementum ultricies at
-      nec urna. Nulla facilisi.
-    </p>
-    <p>
-      Quisque tincidunt, nunc in efficitur congue, orci urna ultrices nisl, vel tincidunt
-      tortor sapien non mi. Proin facilisis nisi et purus congue, in eleifend lorem volutpat.
-      Curabitur at purus sit amet ligula iaculis tincidunt.
-    </p>
     <Image
       src="https://bit.ly/placeholder-img"
       alt="image placeholder"
@@ -34,6 +20,26 @@ const PostTemplate = () => (
       height={32}
       quality={75}
     />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi sit amet felis
+      fringilla, a consequat elit cursus. Fusce et magna eget libero elementum ultricies at
+      nec urna. Nulla facilisi.
+    </p>
+    <p>
+      Quisque tincidunt, nunc in efficitur congue, orci urna ultrices nisl, vel tincidunt
+      tortor sapien non mi. Proin facilisis nisi et purus congue, in eleifend lorem volutpat.
+      Curabitur at purus sit amet ligula iaculis tincidunt.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nisi sit amet felis
+      fringilla, a consequat elit cursus. Fusce et magna eget libero elementum ultricies at
+      nec urna. Nulla facilisi.
+    </p>
+    <p>
+      Quisque tincidunt, nunc in efficitur congue, orci urna ultrices nisl, vel tincidunt
+      tortor sapien non mi. Proin facilisis nisi et purus congue, in eleifend lorem volutpat.
+      Curabitur at purus sit amet ligula iaculis tincidunt.
+    </p>
   </article>
 </>
 )
