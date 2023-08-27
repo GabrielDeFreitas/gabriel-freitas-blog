fb567462bfa5c5039be2bd251313cf4d766aa4d5
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-08-27 15:03:18 -0300

    config SEO Pages

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -1,4 +1,4 @@
-import LinkWrapper from '@/components/Link'
+import { NextSeo } from 'next-seo'
 
 export type PageTemplateProps = {
   heading: string
@@ -7,11 +7,29 @@ export type PageTemplateProps = {
 
 const PageTemplate = ({ heading, body }: PageTemplateProps) => (
 <>
-  <LinkWrapper href="/">
-    <h2>
-      <span>Return</span>
-    </h2>
-  </LinkWrapper>
+  <NextSeo
+    title={`Gabriel Freitas | Blog - ${heading}`}
+    description={
+      heading ||
+      'A simple study blog project.'
+    }
+    canonical="https://gabrielfreitas.blog.com.br"
+    openGraph={{
+    url: 'https://gabrielfreitas.blog.com.br',
+    title: `Gabriel Freitas | Blog - ${heading}`,
+    description:
+      heading ||
+      'A simple study blog project.',
+      images: [
+        {
+          url: 'https://gabrielfreitas.blog.com.br/img/cover.png',
+          width: 1280,
+          height: 720,
+          alt: 'Gabriel Freitas | Blog'
+        }
+      ],
+    }}
+  />
   <article className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
     <h1>{heading}</h1>
     <div dangerouslySetInnerHTML={{ __html: body }} />
