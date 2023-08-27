7014c04b2ea4915c3b2bb513e2f8ad3df32d99ba
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-08-26 22:21:14 -0300

    add loader NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+          <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
+    <Component {...pageProps} />
+  </>
 }
