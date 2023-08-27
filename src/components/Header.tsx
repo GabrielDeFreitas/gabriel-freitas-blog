7a2491452eee39cb0c9c6468daa110300cf3af10
gabriel freitas <gabrielfreitas.ilustra@gmail.com>
2023-08-27 16:34:30 -0300

    styles pages templates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ const Header = () => (
           <nav className="ml-auto text-sm font-medium space-x-6">
             <Link className="hover:underline focus:outline-offset-8 focus:outline-black-700 visited:text-wai-green" href="/">Home</Link>
             <Link className="hover:underline focus:outline-offset-8 focus:outline-black-700 visited:text-wai-green" href="/about">About</Link>
+            <Link className="hover:underline focus:outline-offset-8 focus:outline-black-700 visited:text-wai-green" href="/contact">Contact</Link>
           </nav>
       </div>
     </header>
--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -1,3 +1,4 @@
+import Contact from '@/components/Contact'
 import { NextSeo } from 'next-seo'
 
 export type PageTemplateProps = {
@@ -30,10 +31,15 @@ const PageTemplate = ({ heading, body }: PageTemplateProps) => (
       ],
     }}
   />
-  <article className="flex gap-4 flex-col p-8 bg-white border border-gray-300 rounded">
-    <h1>{heading}</h1>
-    <div dangerouslySetInnerHTML={{ __html: body }} />
-  </article>
+  <main className="max-w-2xl mx-auto py-10 px-4">
+    <article className="py-6 prose dark:prose-invert">
+      <h1 className="mb-1 md:mb-6 font-medium text-lg">{heading}</h1>
+      <hr className="my-4"/>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+      <hr className="my-4"/>
+    </article>
+  </main>
+  <Contact/>
 </>
 )
 
